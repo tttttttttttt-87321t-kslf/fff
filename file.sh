6f6c35326d9033d70c5a62d75f1d535da72cf695
null <onerjjy@gmail.com>
2025-10-03 12:33:15 +0330

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,57 +1,65 @@
 #!/bin/bash
 
 SERVER_ROLE=$1
+PEER_ROLE=$([[ "$SERVER_ROLE" == "A" ]] && echo "B" || echo "A")
+
 if [[ -z "$SERVER_ROLE" ]]; then
   echo "Usage: $0 <A|B>"
   exit 1
 fi
 
 WG_PORT=51820
-
 WG_DIR=/opt/wireguard
 WG_CONF_DIR=$WG_DIR/config/wg_confs
 
-if [[ "$SERVER_ROLE" == "A" ]]; then
-    WG_IP="10.8.0.1/24"
-    PEER_IP="10.8.0.2/24"
-else
-    WG_IP="10.8.0.2/24"
-    PEER_IP="10.8.0.1/24"
-fi
+declare -A WG_IP
+WG_IP[A]="10.8.0.1/24"
+WG_IP[B]="10.8.0.2/24"
 
+# Clean up previous config
 sudo docker compose down 2>/dev/null
 sudo rm -rf $WG_DIR/config/*
 
 sudo mkdir -p $WG_CONF_DIR/server$SERVER_ROLE
 
+# Generate keys
 PRIVATE_KEY=$(wg genkey)
 PUBLIC_KEY=$(echo "$PRIVATE_KEY" | wg pubkey)
 PRESHARED_KEY=$(wg genpsk)
-
-echo "[*] Private key: $PRIVATE_KEY"
-echo "[*] Public key: $PUBLIC_KEY"
-echo "[*] Preshared key: $PRESHARED_KEY"
 
 echo "$PRIVATE_KEY" > $WG_CONF_DIR/server$SERVER_ROLE/privatekey
 echo "$PUBLIC_KEY" > $WG_CONF_DIR/server$SERVER_ROLE/publickey
 echo "$PRESHARED_KEY" > $WG_CONF_DIR/server$SERVER_ROLE/presharedkey
 
+# Save peer info to tmp files for cross-server usage
+echo "$PUBLIC_KEY" > $WG_DIR/$SERVER_ROLE.public
+echo "$PRESHARED_KEY" > $WG_DIR/$SERVER_ROLE.psk
+
+# Wait until peer files exist
+echo "[*] Waiting for peer keys..."
+while [ ! -f $WG_DIR/$PEER_ROLE.public ] || [ ! -f $WG_DIR/$PEER_ROLE.psk ]; do
+    sleep 1
+done
+
+PEER_PUBLIC_KEY=$(cat $WG_DIR/$PEER_ROLE.public)
+PEER_PSK=$(cat $WG_DIR/$PEER_ROLE.psk)
+
 cat <<EOF | sudo tee $WG_CONF_DIR/server$SERVER_ROLE/wg0.conf
 [Interface]
-Address = $WG_IP
+Address = ${WG_IP[$SERVER_ROLE]}
 PrivateKey = $PRIVATE_KEY
 ListenPort = $WG_PORT
 DNS = 10.8.0.1
 
-# Peer configuration will be added manually later
-# [Peer]
-# PublicKey = <Peer_PublicKey>
-# PresharedKey = <PresharedKey>
-# Endpoint = <Peer_PublicIP>:51820
-# AllowedIPs = <Peer_InternalIP>/32
-# PersistentKeepalive = 25
+[Peer]
+PublicKey = $PEER_PUBLIC_KEY
+PresharedKey = $PEER_PSK
+Endpoint = <PEER_PUBLIC_IP>:$WG_PORT
+AllowedIPs = ${WG_IP[$PEER_ROLE]}/32
+PersistentKeepalive = 25
 EOF
 
+# docker-compose.yml
 cat <<EOF | sudo tee $WG_DIR/docker-compose.yml
 version: '3.8'
 services:
@@ -72,8 +80,8 @@ services:
     restart: unless-stopped
 EOF
 
-echo "[*]  WireGuard..."
+# Launch
 cd $WG_DIR
 sudo docker compose up -d
 
-echo "[*] $WG_CONF_DIR/server$SERVER_ROLE/wg0.conf"
+echo "[*] WireGuard Server $SERVER_ROLE is up!"
